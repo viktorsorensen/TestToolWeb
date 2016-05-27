787742211526bd3944ad20e34abe4ab98b6e0bf2
viktorsorensen <viktor.sorensen87@outlook.com>
2016-05-27 14:13:42 +0200

    Hejhej

--- teacher/script.js
+++ teacher/script.js
@@ -1,5 +1,44 @@
-$("#openquestionbutton").click(function() {
-	$("#openquestion").toggle("slow", function() {
-
-	});
+$(document).ready(function() {
+    $("#add").click(function() {
+        var intId = $("#buildyourform div").length + 1;
+        var fieldWrapper = $("<div class=\"fieldwrapper\" id=\"field" + intId + "\"/>");
+        var fName = $("<input type=\"text\" class=\"fieldname\" />");
+        var fType = $("<select class=\"fieldtype\"><option value=\"checkbox\">Flervals</option><option value=\"textbox\">Öppen fråga</option><option value=\"radio\">Alternativfråga</option><option value=\"rank\">Ranked</option></select>");
+        var removeButton = $("<input type=\"button\" class=\"remove\" value=\"-\" />");
+        removeButton.click(function() {
+            $(this).parent().remove();
+        });
+        fieldWrapper.append(fName);
+        fieldWrapper.append(fType);
+        fieldWrapper.append(removeButton);
+        $("#buildyourform").append(fieldWrapper);
+    });
+    $("#preview").click(function() {
+        $("#yourform").remove();
+        var fieldSet = $("<fieldset id=\"yourform\"><legend>Prov</legend></fieldset>");
+        $("#buildyourform div").each(function() {
+            var id = "input" + $(this).attr("id").replace("field","");
+            var label = $("<label for=\"" + id + "\">" + $(this).find("input.fieldname").first().val() + "</label>");
+            var input;
+            switch ($(this).find("select.fieldtype").first().val()) {
+                case "checkbox":
+                    input = $("<input type=\"checkbox\" id=\"" + id + "\" name=\"" + id + "\" />");
+                    break;
+                case "textbox":
+                    input = $("<input type=\"text\" id=\"" + id + "\" name=\"" + id + "\" />");
+                    break;
+                case "radio":
+                    input = $("<input type=\"radio\" id=\"" + id + "\" name=\"" + id + "\" />");
+                    break;
+                case "rank":
+                	input = $("<select><option value=\"1\">1</option><option value=\"2\">2</option><option value=\"3\">3</option><option value=\"4\">4</option>");
+                	
+                	break;
+            }
+            fieldSet.append(label);
+            fieldSet.append(input);
+        });
+        $(".middle").append(fieldSet);
+    });
 });
+
