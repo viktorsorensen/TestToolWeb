2a64f782e83c7b3b14942130d7f3a43659dfb324
viktorsorensen <viktor.sorensen87@outlook.com>
2016-06-01 14:32:36 +0200

    Changed formatting

--- teacher/script.js
+++ teacher/script.js
@@ -1,11 +1,11 @@
-$(document).ready(function() {
-    $("#add").click(function() {
+$(document).ready(function () {
+    $("#add").click(function () {
         var intId = $("#buildyourform div").length + 1;
         var fieldWrapper = $("<div class=\"fieldwrapper\" id=\"field" + intId + "\"/>");
         var fName = $("<input type=\"text\" class=\"fieldname\" />");
         var fType = $("<select class=\"fieldtype\"><option value=\"checkbox\">Flervals</option><option value=\"textbox\">Öppen fråga</option><option value=\"radio\">Alternativfråga</option><option value=\"rank\">Ranked</option></select>");
         var removeButton = $("<input type=\"button\" class=\"remove\" value=\"-\" />");
-        removeButton.click(function() {
+        removeButton.click(function () {
             $(this).parent().remove();
         });
         fieldWrapper.append(fName);
@@ -13,11 +13,11 @@ $(document).ready(function() {
         fieldWrapper.append(removeButton);
         $("#buildyourform").append(fieldWrapper);
     });
-    $("#preview").click(function() {
+    $("#preview").click(function () {
         $("#yourform").remove();
         var fieldSet = $("<fieldset id=\"yourform\"><legend>Prov</legend></fieldset>");
-        $("#buildyourform div").each(function() {
-            var id = "input" + $(this).attr("id").replace("field","");
+        $("#buildyourform div").each(function () {
+            var id = "input" + $(this).attr("id").replace("field", "");
             var label = $("<label for=\"" + id + "\">" + $(this).find("input.fieldname").first().val() + "</label>");
             var input;
             switch ($(this).find("select.fieldtype").first().val()) {
@@ -31,9 +31,9 @@ $(document).ready(function() {
                     input = $("<input type=\"radio\" id=\"" + id + "\" name=\"" + id + "\" />");
                     break;
                 case "rank":
-                	input = $("<select><option value=\"1\">1</option><option value=\"2\">2</option><option value=\"3\">3</option><option value=\"4\">4</option>");
-                	
-                	break;
+                    input = $("<select><option value=\"1\">1</option><option value=\"2\">2</option><option value=\"3\">3</option><option value=\"4\">4</option>");
+
+                    break;
             }
             fieldSet.append(label);
             fieldSet.append(input);
